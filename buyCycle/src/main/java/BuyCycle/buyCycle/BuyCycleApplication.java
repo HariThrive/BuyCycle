fa9d3433070e79a9f64339bfa38051b17b468b84
hariram <hariram@gain>
2025-03-21 11:48:35 +0530

    initial commit

---
+++ buyCycle/src/main/java/BuyCycle/buyCycle/BuyCycleApplication.java
@@ -0,0 +1,13 @@
+package BuyCycle.buyCycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyCycleApplication.class, args);
+	}
+
+}
---
+++ buyCycle/src/test/java/BuyCycle/buyCycle/BuyCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package BuyCycle.buyCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
